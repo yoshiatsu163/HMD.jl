fd305010ea40c916d1aece1997fcffc2e75dbb3b
Documenter.jl <documenter@juliadocs.github.io>
2022-08-27 07:22:20 +0000

    build based on 2617291

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HMD","category":"page"},{"location":"#HMD","page":"Home","title":"HMD","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HMD.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HMD]","category":"page"}]
+}
